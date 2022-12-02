6395b6d8a5dc2f4e0c9f655e43e60e1ae91716b8
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2022-12-02 11:45:18 +0530

    Updated Footer

--- vulnscan.sh
+++ vulnscan.sh
@@ -7,25 +7,35 @@ echo "Checking if Binaries are installed or not"
 if which nuclei >/dev/null; then
     echo "Nuclei found"
 else
-    echo "Nuclei not found"
+    echo "Nuclei not found, please download using following command"
+    echo "go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest"
 fi
 if which katana 2>&1 ; then
-    echo "Katana found"
+   echo "Katana found"
 else
-    echo "Katana not found"
+    echo "Katana not found, please download using following command"
+    echo "go install github.com/projectdiscovery/katana/cmd/katana@latest"
 fi
 if which httpx 2>&1 ; then
     echo "httpx found"
 else
-    echo "httpx not found"
+    echo "httpx not found, please download using following command"
+    echo "go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
 fi
 if which sublist3r 2>&1 ; then
-    echo "Sublist3r found"
+    echo "Subfinder found"
 else
-    echo "Sublist3r not found"
+    echo "Subfinder not found, please download using following command"
+    echo "go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
 fi
-echo "====================================================================================================================================="
+echo "====================================================================================================================================================="
 echo "Developed by Raziq"
-echo "====================================================================================================================================="
+echo "====================================================================================================================================================="
 echo "Running Scan on" $urlname
 subfinder -d $urlname >> subdomains.txt; httpx --silent subdomains.txt >> alive_domains.txt; katana --silent alive_domains.txt >> endpoints.txt; nuclei -l endpoints.txt -o nuclei_output.txt
+echo "====================================================================================================================================================="
+echo "Scanning Completed, results are saved as below"
+echo "Subfinder: subdomains.txt"
+echo "httpx: alive_domains.txt"
+echo "Katana: endpoints.txt"
+echo "Nuclei: nuclei_output.txt"
